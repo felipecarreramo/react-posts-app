7428d520fb27eff72a77a7ae4ac027ea5d789253
Juan Felipe Carrera <juanfelipecarrera@Juans-MacBook-Pro.local>
2019-03-07 02:50:52 -0500

    add post action

--- src/Redux/Posts.js
+++ src/Redux/Posts.js
@@ -6,7 +6,18 @@ const INITIAL_STATE = Immutable({
 
 export const reducer = function(state = INITIAL_STATE, action) {
   switch (action.type) {
+    case 'ADD_POST':
+      return state.merge({ items: [...state.items, action.post] })
     default:
       return state
   }
+}
+
+export const actions = {
+  addPost: function (post) {
+    return {
+      type: 'ADD_POST',
+      post,
+    }
+  },
 }
--- src/index.js
+++ src/index.js
@@ -5,8 +5,11 @@ import App from './App';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import store from './Redux'
+import { actions as PostsActions } from './Redux/Posts'
+
+let createdStore = createStore(store)
 
 ReactDOM.render(
-  <Provider store={ createStore(store) }>
+  <Provider store={ createdStore }>
     <App />
   </Provider>, document.getElementById('root'));
