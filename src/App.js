f9856ab1700986d48d2c45de88eb7572f158f412
Juan Felipe Carrera <juanfelipecarrera@Juans-MacBook-Pro.local>
2019-03-07 03:03:44 -0500

    add post view

--- src/App.js
+++ src/App.js
@@ -2,10 +2,49 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    newPostTitle: "",
+    newPostBody: "",
+  }
+
+  createNewPost() {
+    let post = {
+      title: this.state.newPostTitle,
+      body: this.state.newPostBody
+    }
+
+    console.log(post)
+  }
+
   render() {
     return (
       <div className="App">
-      
+
+        <div>New Post</div>
+        <div>
+          <div>
+            <input
+              type="text"
+              placeholder="Title"
+              value={ this.state.newPostTitle }
+              onChange={ event => this.setState({ newPostTitle: event.target.value }) } />
+          </div>
+          <div>
+            <input
+              type="text"
+              placeholder="Body"
+              value={ this.state.newPostBody }
+              onChange={ event => this.setState({ newPostBody: event.target.value }) } />
+          </div>
+          <div>
+            <button
+              onClick={ () => this.createNewPost() }>
+              New Post
+            </button>
+          </div>
+        </div>
+
       </div>
     );
   }
